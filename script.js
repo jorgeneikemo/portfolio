1bc000c0c051da06cbbf6fd0d4410b61e805634a
jorgen eikemo <jorgen.eikemo@gmail.com>
2023-05-22 21:20:21 +0200

    complete rework

--- script.js
+++ script.js
@@ -1,9 +1,2 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const projectsSection = document.getElementById("projects");
-    const linkToProjects = document.querySelector("a[href='#projects']");
-
-    linkToProjects.addEventListener("click", function (event) {
-      event.preventDefault();
-      projectsSection.scrollIntoView({ behavior: "smooth" });
-    });
   });
